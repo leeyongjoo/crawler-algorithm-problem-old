de78ec396dc05a1ba808ee752a2ac79c6206230e
leeyongjoo <2yongjooo@gmail.com>
2020-03-23 14:04:07 +0900

    Upload 1 File

---
+++ programmers/level1/[해시] 완주하지 못한 선수.py
@@ -0,0 +1,9 @@
+# [해시] 완주하지 못한 선수
+def solution(participant, completion):
+    answer = ''
+    return answer
+    
+if __name__ == "__main__":
+    print(solution(["leo", "kiki", "eden"], ["eden", "kiki"]) == "leo")
+    print(solution(["marina", "josipa", "nikola", "vinko", "filipa"], ["josipa", "filipa", "marina", "nikola"]) == "vinko")
+    print(solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"]) == "mislav")
