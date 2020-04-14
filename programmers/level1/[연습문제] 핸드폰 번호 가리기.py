d2ebf63e14a4182d984c671dd799a18182df0309
leeyongjoo <2yongjooo@gmail.com>
2020-04-14 17:57:47 +0900

    Solve lv1 [연습문제] 핸드폰 번호 가리기.py

---
+++ programmers/level1/[연습문제] 핸드폰 번호 가리기.py
@@ -0,0 +1,9 @@
+# [연습문제] 핸드폰 번호 가리기
+def solution(phone_number):
+    return '*'*(len(phone_number)-4) + phone_number[-4:]
+    
+if __name__ == "__main__":
+    print(solution("01033334444"))
+    print(solution("01033334444") == "*******4444")
+    print(solution("027778888"))
+    print(solution("027778888") == "*****8888")
