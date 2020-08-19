c3360ad70927260c108370aa4f834babcefe3a54
leeyongjoo <2yongjooo@gmail.com>
2020-08-19 20:29:53 +0900

    feat: get_file_list 함수 추가

--- modules/module_path.py
+++ modules/module_path.py
@@ -1,5 +1,25 @@
 import os
 
-def mkdir_if_not_exists(dirname):
-    if os.path.isdir(dirname) is False:
-        os.mkdir(dirname)
+
+def mkdir_if_not_exists(path_dir):
+    while True:
+        try:
+            if os.path.isdir(path_dir) is False:
+                os.mkdir(path_dir)
+        except FileNotFoundError:
+            print(os.path.abspath(os.path.join(path_dir, os.pardir)))
+            mkdir_if_not_exists(
+                os.path.abspath(os.path.join(path_dir, os.pardir)))
+        else:
+            break
+
+
+def get_file_list(path_dir):
+    if os.path.isdir(path_dir) is True:
+        return os.listdir(path_dir)
+    else:
+        return []
+
+
+if __name__=="__main__":
+    print(join_path('1','2'))
